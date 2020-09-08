2e878c5f821131abc9cbb396252879ab4df9cd55
gobal <45533772+satheeshkumarsoft@users.noreply.github.com>
2020-09-08 12:46:54 +0530

    Revert "fix post issue"
    This reverts commit b096a7f5f5ac4a20b8558a29775153149bfe4683.

--- SMSApi/App_Start/WebApiConfig.cs
+++ SMSApi/App_Start/WebApiConfig.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
-using System.Web.Http.Cors;
-using Newtonsoft.Json.Serialization;
 
 namespace SchoolMSApi
 {
@@ -21,15 +19,6 @@ namespace SchoolMSApi
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
-            var cors = new EnableCorsAttribute("http://localhost:4200", "*", "*");
-            config.EnableCors(cors);
-
-            // Set JSON formatter as default one and remove XmlFormatter
-
-            var jsonFormatter = config.Formatters.JsonFormatter;
-            jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
-            config.Formatters.Remove(config.Formatters.XmlFormatter);
-            jsonFormatter.SerializerSettings.DateTimeZoneHandling = Newtonsoft.Json.DateTimeZoneHandling.Utc;
         }
     }
 }
